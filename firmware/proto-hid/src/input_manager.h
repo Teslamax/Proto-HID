3fd1f3b33829c291d3d50d98b842ccb0e75c0b34
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-04-29 04:58:24 -0500

    Update input_manager.h

--- firmware/proto-hid/src/input_manager.h
+++ firmware/proto-hid/src/input_manager.h
@@ -1,3 +1,4 @@
+// input_manager.h - Handles I2C rotary encoders and keypad input for Proto-HID
 #pragma once
 
 // Initialize I2C devices (encoders, keypad)
@@ -5,3 +6,6 @@ void init_inputs();
 
 // Poll I2C devices and process inputs (rotation, button presses)
 void update_inputs();
+
+// Print current input states (for /input CLI command)
+void print_inputs();
