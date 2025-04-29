eea4cf83b71d7988d3504e24f7e4f1905ecb4455
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-04-29 04:50:14 -0500

    Update cli_parser.h

--- firmware/proto-hid/src/cli_parser.h
+++ firmware/proto-hid/src/cli_parser.h
@@ -1,12 +1,13 @@
+// cli_parser.h - Command line parser for serial input
 #pragma once
 
 // Initialize CLI parser
 void cli_parser_init();
 
-// Periodically called in main loop to parse serial input
+// Poll and process CLI input
 void cli_parser_update();
 
-// Logging functions (respect CDC log level from config.h)
+// Logging functions
 void logInfo(const char* fmt, ...);
 void logWarn(const char* fmt, ...);
 void logError(const char* fmt, ...);
