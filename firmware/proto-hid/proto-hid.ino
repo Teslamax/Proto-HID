570551b77bd2418634c526b40d2f0578baef4fcf
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-04-29 02:57:42 -0500

    Update proto-hid.ino

--- firmware/proto-hid/proto-hid.ino
+++ firmware/proto-hid/proto-hid.ino
@@ -1,4 +1,20 @@
-// proto-hid.ino - main firmware entry point
+// proto-hid.ino - Main sketch for Proto-HID controller
+// Hardware: SparkFun Pro Micro (ATmega32U4), Adafruit Seesaw I2C devices
+// Function: USB MIDI + CLI control using rotary encoders + NeoKey 1x4
 
-void setup() {}
-void loop() {}
+#include "src/input_manager.h"
+#include "src/midi_manager.h"
+#include "src/cli_parser.h"
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial); // Wait for USB Serial
+
+  cli_parser_init();
+  init_inputs();
+}
+
+void loop() {
+  update_inputs();
+  cli_parser_update();
+}
